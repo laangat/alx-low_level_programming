ae4a133e166cf4e71cf7464d945b2b457a3bda5c
laangat <langatkevoh18@gmail.com>
2022-12-08 03:03:47 -0800

    C-1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * mai - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
