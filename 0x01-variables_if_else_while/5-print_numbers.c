98c12c9e9ea6101b8ddc46b7b2eca922b6596f98
laangat <langatkevoh18@gmail.com>
2022-12-13 13:01:48 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * prints out numbers of base 10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
