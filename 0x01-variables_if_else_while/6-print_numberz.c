7d7bfe7ddec8aa101a44f43537168d3629913546
laangat <langatkevoh18@gmail.com>
2022-12-13 13:30:04 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/* betty style doc for function main goes here */
+/**
+ * main - Entry point
+ *
+ * prints numbers using putchar without using type char
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+	       	(x < 10) {
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
