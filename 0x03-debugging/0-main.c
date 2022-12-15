d2fd93132558809fdb720468965137e96869d9fa
laangat <langatkevoh18@gmail.com>
2022-12-15 13:28:00 -0800

    debug p + or -

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function that tests if
+ * a number is positive or negative
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
