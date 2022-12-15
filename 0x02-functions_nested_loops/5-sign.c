6951d370dc3694c3d344e57dfadbb6b247c571da
laangat <langatkevoh18@gmail.com>
2022-12-14 18:31:29 -0800

    sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#inlcude "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @int_n: number to be checked
+ *
+ * Return:1 if the number is greater than zero
+ * 0 if number is zero
+ * -1 if number is less than zero
+ */
+
+int print_sign(int_n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
