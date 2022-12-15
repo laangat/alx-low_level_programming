221b5ffe2c75faf00eaab4355249e006bf5c229e
laangat <langatkevoh18@gmail.com>
2022-12-14 19:11:42 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ * int_abs(int) - prints the absolute value of an integer
+ *
+ * @int: the number which to be tested
+ *
+ * Return Always 0.
+ */
+
+int_abs(int)
+{
+	int = x;
+	if (x < 0)
+	{
+		return (-x);
+	}
+	else
+	{
+		return (x);
+	}
+}
