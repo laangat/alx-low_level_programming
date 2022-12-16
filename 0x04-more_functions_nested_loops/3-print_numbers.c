5e8bb60764dd10e6367a64e278d81d7c01bc57ef
laangat <langatkevoh18@gmail.com>
2022-12-15 17:16:38 -0800

    numbers 0to9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: numbers 0 to 9
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+		_putchar('\n\);
+	}
+	return (0);
+}
