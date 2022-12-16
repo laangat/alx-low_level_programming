3e824b60bb727f06300b433c7d0fe6af6cd5d3c6
laangat <langatkevoh18@gmail.com>
2022-12-16 11:42:32 -0800

    more no's

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 and 14
+ * 10 times
+ *
+ * Return: 10 lines of numbers between 0 and 14
+ */
+
+void more_numbers(void)
+{
+	int k;
+	int j;
+
+	for (k = 0; k <= 9; k++)
+	{
+		for (j = 0; j <= 10; j++)
+		{
+			_putchar('0' + i);
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
