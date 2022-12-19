3a9294f899e5409d3aa64fe19c995d02ffdd5543
laangat <langatkevoh18@gmail.com>
2022-12-19 09:16:39 -0800

    Reset pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,14 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an int and updates it
+ * reset_to_98 - takes a pointer to an int as a parameter
+ * and updates the value it points to 98
  *
- * @n: variable of int
+ * @n: This is entry
  * Return: updated value of variable n
  */
 
 void reset_to_98(int *n)
 {
-	int n = 402;
 	*n = 98;
-
-	putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+
+void swap_int(int *a, int *b);
+
+int main(void)
+{
+	int temp = *a;
+
+	*a = *b;
+	_putchar(
+	*b = temp;
