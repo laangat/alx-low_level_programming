2d6e3ac4130db1213a69a7d71b0bdf095441eabf
laangat <langatkevoh18@gmail.com>
2022-12-19 11:48:55 -0800

    String length

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints string followed by a newline
+ *
+ * @str: string to input
+ *  Return: string and newline
+ */
+
+void _puts(char *str)
+{
+
