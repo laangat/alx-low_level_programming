febf16d4c92029f379fc0fdcc6544d07a3657397
laangat <langatkevoh18@gmail.com>
2022-12-19 17:57:12 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reverse string
+ * @s: input string
+ *
+ * Return: a string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	for (i = i - 1; s[i] != '\0'; i--)
+	{
+		_putschar(s[i]);
+	}
+
+	_putschar('\n');
+}
