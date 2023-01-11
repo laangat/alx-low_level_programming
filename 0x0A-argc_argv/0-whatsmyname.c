52524310f1e8124f1b79fa7f7903251f4d602d9d
laangat <langatkevoh18@gmail.com>
2023-01-10 19:52:44 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argumet count.
+ * @argv: argument vector
+ * Return: Name of program.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
