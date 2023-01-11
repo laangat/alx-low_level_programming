552b30d8e048a9745f4a87f47968fa85cdaab62c
laangat <langatkevoh18@gmail.com>
2023-01-10 22:59:51 -0800

    Multiply numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string into and integer
+ * _atoi - converts a string to an integer
  *
  * @s: string to be converted
  *
- * Return: the int converted from string
+ * Return: the int converted from the string
+ *
  */
 
 int _atoi(char *s)
@@ -40,12 +41,12 @@ int _atoi(char *s)
 			f = 1;
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-
 				break;
+
 			f = 0;
 		}
 
-		i++;
+	i++;
 	}
 
 	if (f == 0)
@@ -61,26 +62,27 @@ int _atoi(char *s)
  *
  * @argv: array of arguments
  *
- * Return: 0 (success), 1 (Error)
+ * Return: 0 (Success), 1 (Error)
+ *
  */
-
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
-		printf("Error");
-
+		printf("Error\n");
 		return (1);
 	}
 
 	num1 = _atoi(argv[1]);
+
 	num2 = _atoi(argv[2]);
 
-	result = num1 + num2;
+	result = num1 * num2;
 
 	printf("%d\n", result);
 
 	return (0);
+
 }
